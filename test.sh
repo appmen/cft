cf50f6c4b6b0affc652d7b9d503ca412eaa80058
Victor Kuvshynov <vkuvshynov@plexteq.com>
2018-10-02 18:28:29 +0300

    Changed launch mode

--- test.sh
+++ test.sh
@@ -1,1 +1,11 @@
-mvn clean test "-Dwebdriver.gecko.driver=driver/geckodriver"
+#!/bin/bash
+
+OS=`uname`
+
+if [[ "$OS" == 'Linux' ]]; then
+   chmod +x driver/geckodriver_linux
+   mvn -Dwebdriver.gecko.driver=driver/geckodriver_linux clean test
+elif [[ "$OS" == 'Darwin' ]]; then
+   chmod +x driver/geckodriver_osx
+   mvn -Dwebdriver.gecko.driver=driver/geckodriver_osx clean test
+fi
