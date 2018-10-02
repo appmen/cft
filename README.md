867e303bf5b78525aff303041910c5c828bce9b5
Victor Kuvshynov <vkuvshynov@plexteq.com>
2018-10-02 16:18:24 +0300

    Drivers are stored in a separate folder for ease of use

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # cft
- - download chromedriver
- - put it into the Downloads folder
- - open project folder in cmd at run "mvn clean test"
+Prerequisites
+
+Make sure you have following prerequisites installed
+- JDK 1.8.x
+- Git
+- Apache Maven 3.x
+
+Run tests:
+ - clone this repo
+ - open project folder in console and run:
+   * test.cmd under Windows
+   * test.sh under Linux/OSX (make sure that test.sh has executable permission, if not, do `chmod +x ./test.sh`)
+   * please use corresponding webdriver: geckodriver.exe for Windows, geckodriver for Mac, and geckodriver_linux for linux (can be found at the `driver` folder)
--- src/test/java/tests/BasicTests.java
+++ src/test/java/tests/BasicTests.java
@@ -3,69 +3,74 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
-import structure.Dashboard;
-import structure.Driver;
-import structure.Login;
 
+import pages.Dashboard;
+import pages.Driver;
+import pages.Login;
+import pages.Logout;
+import utils.FileUtils;
 
-public class BasicTests {
-
-    private WebDriver driver = new Driver().getFFdriver();
+public class BasicTests
+{
+    private WebDriver driver;
+    private static final String tempStorageDir = FileUtils.createTempDir("cft");
     private static final String userName = "Test";
-    private Login login;
-    private Dashboard dashboard;
     private Assertion asserts = new Assertion();
 
-
     @BeforeClass
-    public void setUp(){
-        login = new Login(driver);
-        dashboard = new Dashboard(driver);
-        login.login();
+    public void setup() {
+    	driver = new Driver(tempStorageDir).getFFdriver();
     }
-
+    
+    @AfterClass
+    public void quit(){
+        driver.quit();
+    }
+    
     @BeforeMethod
-    public void openHomepage(){
+    public void beforeTest() {
+        new Login(driver).login();
         driver.get(System.getProperty("url"));
-        System.out.println("before");
     }
-
+    
+    @AfterMethod
+    public void afterTest() {
+    	new Logout(driver).logout();
+    }
+    
     @Test(priority = 1)
-    public void verifyEntityExist(){
+    public void verifyEntityExist() {
+    	Dashboard dashboard = new Dashboard(driver, tempStorageDir);
         dashboard.searchFor(userName);
         asserts.assertTrue (dashboard.getSearchResult(0)[0].contains(userName),
                 "Verify that user name has updated version");
     }
 
     @Test(priority = 2)
-    public void exportEntity(){
+    public void exportEntity() {
+    	Dashboard dashboard = new Dashboard(driver, tempStorageDir);
         dashboard.searchFor(userName);
         dashboard.makeAction(0, "Export");
         dashboard.exportEntity();
-        asserts.assertEquals(dashboard.waitAndGetExportStatus(), "Completed",
-        "Verify ability to export an entity");
+        asserts.assertEquals(dashboard.waitAndGetExportStatus(), "Completed", "Verify ability to export an entity");
     }
 
     @Test(priority = 3)
-    public void downloadExportedEntity(){
+    public void downloadExportedEntity() {
+    	Dashboard dashboard = new Dashboard(driver, tempStorageDir);
         asserts.assertTrue(dashboard.getExportedEntity().contains("FN:"+userName),
                 "Verify that exported file contains correct user name");
     }
     
     @Test(priority = 4)
     public void verifyExportDate() {
+    	Dashboard dashboard = new Dashboard(driver, tempStorageDir);
     	asserts.assertTrue(dashboard.isFirsRecordExportedToday(), 
     			"Verify that first record was exported today");
     }
-
-    @AfterClass
-    public void quit(){
-        driver.quit();
-    }
-
 }
